186b98c313dbb60996ea598c5e7c0a4e8746c889
Luiz Taira <lftaira@gmail.com>
2020-03-20 17:29:50 -0300

    controles basicos xamarins

---
+++ XF.ControlesBasicos/Configuracao.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XF.ControlesBasicos
+{
+    public class Configuracao
+    {
+        public bool ReceberEmail { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ XF.ControlesBasicos/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF.ControlesBasicos
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public Configuracao config;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Enviar_Clicked(object sender, EventArgs e)
+        {
+            if (!config.ReceberEmail)
+                DisplayAlert("Mensagem", "E-mail não autorizado", "Ok");
+            else
+                DisplayAlert("Mensagem", string.Format("E-mail enviado para {0}", config.Email), "Ok");
+        }
+
+        void Configuracao_Clicked(object sender, EventArgs e)
+        {
+            if (config == null)
+                config = new Configuracao();
+
+            Navigation.PushAsync(new ConfigPage() { BindingContext = config });
+
+        }
+    }
+}
